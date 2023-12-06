e9e9ac011ebd24e7ef53aebdcae4681fd64055b7
Ulyssem23 <152270452+Ulyssem23@users.noreply.github.com>
2023-12-06 22:36:10 +0100

    Update ball.py
    Changed the Y axis so that the ball bounces properly off the paddle

--- ball.py
+++ ball.py
@@ -1,65 +1,66 @@
-"""import turtle
-from turtle import Turtle
-
-#we start by setting the move distance outside of the class indent
-move_dist = 10
-
-class Ball(Turtle):
-    def __init__(ball):
-        super().__init__() #we want to retrieve all properties from the master class so we use the super() function, and intit() applies is to the ball
-        ball.shape('circle') #set the shape of the ball to a circle
-        ball.colour('white') #set the colour of the ball to white bc the screen bg colour is black
-
-#because the screen is set on a x,y space, we need to define the movement of the ball according to the x and y axis:
-        ball.move_dist_x = move_dist
-        ball.move_dist_y = move_dist #the moving distance on both x and y every time the ball moves is 10 steps
-        ball.goto(-50, 300) #we set the INITIAL POSITION of the ball (change selon screen)
-        ball.reset() #the ball is ready to be in the game
-
-#we start a new block for speed and movement of the block once the game has started:
-    def movement(ball):     
-        ball.move_speed = 0.05 #we set the speed at which the ball will move
-#we need to define the movement of the ball according to the x and y axis
-        ball.movement = ball.move_dist_x + ball.move_dist_y
-
-
-#we start a new block to study the case in which the ball bounces on a brick, on a boundary/wall, or on the paddle:
-        def bounce(ball, x_bounce, y_bounce):
-       #we start an if loop for if the ball meets an object:
-                if x_bounce:
-                     ball.move_dist_x *=-1
-                elif y_bounce:
-                      ball.move_dist_y *=-1
-        
-#we start a new block to reset the ball's position every time the game starts or restarts:
-        def reset(ball):
-             ball.goto(x= 50, y= 820)
-             ball.move_dist_y = 10"""
-        
 import turtle
 from turtle import Turtle
+import math
+import random
 
 class Ball(Turtle):
-    def __init__(self):
-        super().__init__()  
+    def __init__(self, paddle):
+        super().__init__()
         self.shape('circle')
         self.penup()
-        self.color('white')  # Set the color to white
-        self.goto(0, -150)
-        self.move_dist_x = 10
-        self.move_dist_y = 10
+        self.color('white')
+        self.goto(10, -150)
+        self.move_dist_x = 0
+        self.move_dist_y = -10
         self.move_speed = 0.05
+        self.paddle = paddle  # Store the paddle instance
 
     def movement(self):
-        # Move the ball according to the x and y axis
         new_x = self.xcor() + self.move_dist_x
         new_y = self.ycor() + self.move_dist_y
         self.goto(new_x, new_y)
 
-    def reset_position(self):
-        # Reset the ball's position to the initial position
-        self.goto(-50, 300)
+        # Check for collisions with walls
+        if new_x > 800 or new_x < -800:
+            self.bounce_x()
+
+        if new_y > 450 or new_y < -450:
+            self.bounce_y()
+
+        # Check for collisions with the paddle
+        if (
+            new_y < -250
+            and self.paddle.xcor() - 50 < new_x < self.paddle.xcor() + 50
+        ):
+            self.bounce_y()
+
+    def bounce_x(self):
+        self.move_dist_x *= -1
+
+    def bounce_y(self):
+        # Calculate the current angle of movement
+        current_angle = math.degrees(math.atan2(self.move_dist_y, self.move_dist_x))
+
+        # Check if the ball is hitting the left or right wall
+        if self.xcor() > 0:  # Ball is on the right side of the screen
+            bounce_angle_degrees = 240
+        else:  # Ball is on the left side of the screen
+            bounce_angle_degrees = 300
+
+        # If the ball is hitting a wall, set the bounce angle to 180 degrees
+        if abs(self.ycor()) > 450:
+            bounce_angle_degrees = 180
+
+        # Calculate the new angle after bouncing
+        new_angle = 2 * bounce_angle_degrees - current_angle
+
+        # Convert the angle back to radians
+        new_angle_radians = math.radians(new_angle)
+
+        # Calculate the new x and y components based on the new angle
+        magnitude = math.sqrt(self.move_dist_x**2 + self.move_dist_y**2)
+        self.move_dist_x = magnitude * math.cos(new_angle_radians)
+        self.move_dist_y = magnitude * math.sin(new_angle_radians)
 
     def start(self):
-        # Add any initialization logic needed when the game starts or restarts
-        pass
+        self.move_dist_y = -10  # Start moving downward
